72fc795ea6fef0d98ce2e5433ceceeb37e7b3dce
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2023-06-14 11:20:36 -0300

    agrugué gitignore a development

--- api/src/db.js
+++ api/src/db.js
@@ -3,9 +3,6 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-// const admin = require('./models/admin');
-// const bussi = require('./models/bussi');
-// const persol = require('./models/persol');
 
 
 const sequelize = new Sequelize(
@@ -20,37 +17,31 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-//? Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+//* Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+//* Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
+//* En sequelize.models están todos los modelos importados como propiedades
+//* Para relacionarlos hacemos un destructuring
 const { Comments, Posts, Roles, User_data, Users } = sequelize.models;
 
-// Aca vendrian las relaciones y la creacion de la tabla intermedia a traves de country_activity
-// Country.belongsToMany(Activity, { through: "country_activity" });
-// Activity.belongsToMany(Country, { through: "country_activity" });
-
-
-// Aca vendrian las relaciones y la creacion de la tabla intermedia
-
+//? Aca vendrian las relaciones y la creacion de la tabla intermedia
 Users.hasOne(User_data, { foreignKey: 'id_users' });
 Roles.hasOne(User_data, { foreignKey: 'id_roles' });
 User_data.hasMany(Posts, { foreignKey: 'id_user_data' })
 Posts.hasMany(Comments, { foreignKey: 'id_post' })
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así
+  ...sequelize.models, 
   conn: sequelize,     // para importar la conexión { conn } = require('./db.js');
 };
