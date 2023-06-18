14572787d3bf33dc4d6328205751d6c24dbf2f41
JuanQuintero1511 <juanrodriguezq1511@gmail.com>
2023-06-17 22:20:47 -0500

    Funcinamiento correcto de commentsControllers

--- api/src/controllers/commentsControllers/commentsControllers.js
+++ api/src/controllers/commentsControllers/commentsControllers.js
@@ -6,34 +6,22 @@ const createNewComment = async (description) => {
 }
 
 const getCommentById = async (id) => {
-    try{
         const commentsById = await Comments.findByPk({id})
         return commentsById;
-    } catch (error) {
-        throw new Error ('El comentario no ha sido encontrado' + error.message);
-    }
 }
 
 const updateComents = async (id, description) => {
-    try {
         const commentsUpdateResult = await Comments.update(
           { description: description },
           { where: { id_coments:  id } }
         );
         return commentsUpdateResult;
-      } catch (error) {
-        throw new Error('Error al actualizar el comentario: ' + error.message);
-      }
 }
 
 
 const deleteComment = async (comment) => {
-    try {
-        const comentDelete = await comment.destroy({})
+        const comentDelete = await Comments.destroy({comment});
         return comentDelete;
-    }catch (error) {
-        throw new Error ('El comentario no ha sido borrado con exito' + error.message);
-    }
 } 
 
 
