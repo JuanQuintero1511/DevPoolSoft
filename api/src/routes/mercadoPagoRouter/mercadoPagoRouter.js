6e04f35e67e5ae45d99af17ce4211f4af681c9dc
JuanQuintero1511 <juanrodriguezq1511@gmail.com>
2023-06-20 14:51:55 -0500

    mercadoPago actualizacion rutas

--- api/src/routes/mercadoPagoRouter/mercadoPagoRouter.js
+++ api/src/routes/mercadoPagoRouter/mercadoPagoRouter.js
@@ -2,6 +2,6 @@ const { Router } = require ("express");
 
 const mercadoPagoRouter = Router();
 
-mercadoPagoRouter.get ("") // Crear pago
-mercadoPagoRouter.delete ("") // Borrar pago 
+mercadoPagoRouter.get ("/") // Crear pago
+mercadoPagoRouter.delete ("/") // Borrar pago 
 
