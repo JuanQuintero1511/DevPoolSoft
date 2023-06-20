e82169e25817717fc39773af2a41521e379f9642
DGonzaArias <adriaramis210@gmail.com>
2023-06-19 23:34:55 -0300

    Agregando Users a la base de datos

--- api/src/controllers/companyControllers/companyControllers.js
+++ api/src/controllers/companyControllers/companyControllers.js
@@ -1,4 +1,4 @@
-const { User_data, Roles, Posts } = require("../../db");
+const { User_data, Roles, Posts, Users } = require("../../db");
 const { Op } = require("sequelize");
 const cloudinary = require("../../utils/cloudinary");
 
@@ -43,70 +43,112 @@ const createCompany = async (
   });
 };
 
+const createCompanyUser = async (userName, email, password) => {
+  await Users.create({
+    userName, email, password
+  })
+}
+
+const setCompanyUsers = async (userName, email, password, full_name) => {
+  const [companyUser, created] = await Users.findOrCreate({
+    where: {
+      userName: `${userName}`,
+      email: `${email}`,
+      password: `${password}`,
+    }
+  })  
+  console.log(companyUser.dataValues)
+  const usersToSet = companyUser.dataValues.id_users
+  await User_data.update({ id_users: `${usersToSet}`}, {
+    where: {
+      full_name: { [Op.iLike]: `%${full_name}%`}
+    }
+  })
+}
+
 
 const setCompanyRol = async (rol_type, full_name) => {
-    const [companyRol, created] = await Roles.findOrCreate({
-        where: { 
-            rol_type: `${rol_type}`}
-    })
-    const atributoToSet = companyRol.dataValues.id_roles
-    await User_data.update({id_roles: `${atributoToSet}`},{
-        where: {
-            full_name: { [Op.iLike]: `%${full_name}%` }
-        }
-    })
-} 
+  const [companyRol, created] = await Roles.findOrCreate({
+    where: {
+      rol_type: `${rol_type}`
+    }
+  })
+  const atributoToSet = companyRol.dataValues.id_roles
+  await User_data.update({ id_roles: `${atributoToSet}` }, {
+    where: {
+      full_name: { [Op.iLike]: `%${full_name}%` }
+    }
+  })
+}
 
 const setCompanyPremium = async (full_name) => {
-    await User_data.update({isPremium: true},{
-        where: {
-            full_name: { [Op.iLike]: `%${full_name}%` }
-        }
-    })
-} 
+  await User_data.update({ isPremium: true }, {
+    where: {
+      full_name: { [Op.iLike]: `%${full_name}%` }
+    }
+  })
+}
 
 
 // //?Trae las empresas de la DB
 const getAllCompanies = async () => {
-    return await User_data.findAll(
+  return await User_data.findAll(
 
-    );
+  );
 };
 
 
 //? Obtiene la empresa por ID especifico mas los posteos
 const getCompanyById = async (id) => {
-      const companyById = await User_data.findByPk(id, {
-        include: {
-          model: Posts,
-          // include: Comment // Incluye los comentarios relacionados con cada post
-        }
-      });     
-      return companyById;
+  const companyById = await User_data.findByPk(id, {
+    include: {
+      model: Posts,
+      // include: Comment // Incluye los comentarios relacionados con cada post
     }
-  
+  });
+  return companyById;
+}
+
 
 //* Obtiene la empresa por nombre
 const searchCompanyByName = async (full_name) => {
-        const companies = await User_data.findAll({
-        where: {
-          full_name: { [Op.iLike]: `%${full_name}%` }
-        },
-        include: {
-          model: Posts
-          // include: Comment 
-        }
-      });
-      return companies;
+  const companies = await User_data.findAll({
+    where: {
+      full_name: { [Op.iLike]: `%${full_name}%` }
+    },
+    include: {
+      model: Posts,      
+      // include: Comment 
+    }
+  });
+  return companies;
+}
+
+const searchUsersByUserName = async (userName) => {
+  const users = await Users.findAll({
+    where: {
+      userName: { [Op.iLike ]: `%${userName}%`}
+    },
+    include: {
+      model: User_data,
+      include: {
+        model: Posts
+      }
+    }
+  })
+  return users
 }
 
 module.exports = {
-    createCompany,
-    setCompanyRol,
-    setCompanyPremium,
-    getAllCompanies,
-    getCompanyById,
-    searchCompanyByName,
+  createCompany,
+  setCompanyRol,
+  createCompanyUser,
+  setCompanyUsers,
+  setCompanyPremium,
+  getAllCompanies,
+  getCompanyById,
+  searchCompanyByName,
+  searchUsersByUserName
 }
 
 
