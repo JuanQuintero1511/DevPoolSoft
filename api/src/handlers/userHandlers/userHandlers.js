7662f87e7f6e9c535f75b563b05c3825a0562e94
Ramiro Fassetta <ramiro.fassetta.01@gmail.com>
2023-06-25 20:53:35 -0300

    userHandler body por query

--- api/src/handlers/userHandlers/userHandlers.js
+++ api/src/handlers/userHandlers/userHandlers.js
@@ -1,8 +1,8 @@
 const { createUser, getAllUsers, searchUsersByUserName, searchUserById } = require("../../controllers/usersControllers/usersControllers")
 
 const getUserHandler = async (req, res) => {
+    const { userName } = req.query; 
     try {
-        const { userName } = req.body; 
         console.log(userName);             
         const results = userName ? await searchUsersByUserName(userName) : await getAllUsers()
         if (results.length === 0) throw Error ({message: "No se encontraron usuarios"})
