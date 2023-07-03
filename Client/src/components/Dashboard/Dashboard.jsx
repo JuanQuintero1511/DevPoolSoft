5eea60e098c18c8b109917c4d9f448fd0d1e5d81
IvoSoria <ivosoria@hotmail.com>
2023-07-03 13:49:51 -0300

    jeje

--- Client/src/components/Dashboard/Dashboard.jsx
+++ Client/src/components/Dashboard/Dashboard.jsx
@@ -20,7 +20,7 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
 const drawerWidth = 240;
-
+//ejje
 const openedMixin = (theme) => ({
   width: drawerWidth,
   transition: theme.transitions.create('width', {
